596337d72e9013d46ddab7c1b0ccffa1c287f3cd
Paras <soodparassood4@gmail.com>
2021-12-18 12:08:23 +0530

    Created models

---
+++ fampay_backend_assignment/backend/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models import fields
+from django.db.models.deletion import CASCADE
+from rest_framework import serializers
+
+# Create your models here.
+class Video(models.Model):
+    video_id=models.CharField(unique=True,max_length=15,blank=False)
+    title=models.CharField(max_length=100,blank=False)
+    description=models.CharField(max_length=5000)
+    publishing_datetime=models.DateTimeField()
+
+class Thumbnail_urls(models.Model):
+    link=models.URLField()
+    video=models.ForeignKey(Video,on_delete=CASCADE,related_name="thumbnails")
+
+class Thumbnail_Serializer(serializers.ModelSerializer):
+    model=Thumbnail_urls
+    class Meta:
+        fields=['id','link']
+
+class Video_Serializer(serializers.ModelSerializer):
+    model=Video
+    thumbnails=Thumbnail_Serializer(many = True)
+    class Meta:
+        fields=['id','video_id','title','description','publishing_datetime','thumbnails']
